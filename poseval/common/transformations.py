f861dc31347472cec29e2ffdd77b4078c75edfff
Jan Zysko <janzysko@gmail.com>
2020-03-19 23:38:46 +0100

    add representation functions for castling, en_passant, and dense representation

--- poseval/common/transformations.py
+++ poseval/common/transformations.py
@@ -1,29 +1,39 @@
-class DataSpecs:
+from typing import Union
+import numpy as np
 
+
+class DataSpecs:
     tensor6x8x8_sparse = 'tensor6x8x8'
     vector6x8x8_flat = 'vector6x8x8'
     tensor12x8x8_sparse = 'tensor12x8x8'
     vector12x8x8_flat = 'vector12x8x8'
 
     piece2layer_for_6x8x8_tensor = {
-        'p': 0,'P': 0,'n': 1,'N': 1,'b': 2,'B': 2,
-        'r': 3,'R': 3,'q': 4,'Q': 4,'k': 5,'K': 5
+        'p': 0, 'P': 0, 'n': 1, 'N': 1, 'b': 2, 'B': 2,
+        'r': 3, 'R': 3, 'q': 4, 'Q': 4, 'k': 5, 'K': 5
     }
 
     piece2value_for_6x8x8_tensor = {
-        'p': 1,'P': -1,'n': 1,'N': -1,'b': 1,'B': -1,
-        'r': 1,'R': -1,'q': 1,'Q': -1,'k': 1,'K': -1
+        'p': 1, 'P': -1, 'n': 1, 'N': -1, 'b': 1, 'B': -1,
+        'r': 1, 'R': -1, 'q': 1, 'Q': -1, 'k': 1, 'K': -1
     }
 
     piece2layer_for_12x8x8_tensor = {
-        'p': 0,'P': 1,'n': 2,'N': 3,'b': 4,'B': 5,
-        'r': 6,'R': 7,'q': 8,'Q': 9,'k': 10,'K': 11
+        'p': 0, 'P': 1, 'n': 2, 'N': 3, 'b': 4, 'B': 5,
+        'r': 6, 'R': 7, 'q': 8, 'Q': 9, 'k': 10, 'K': 11
     }
 
     piece2value_for_12x8x8_tensor = {
-        'p': 1,'P': 1,'n': 1,'N': 1,'b': 1,'B': 1,
-        'r': 1,'R': 1,'q': 1,'Q': 1,'k': 1,'K': 1
+        'p': 1, 'P': 1, 'n': 1, 'N': 1, 'b': 1, 'B': 1,
+        'r': 1, 'R': 1, 'q': 1, 'Q': 1, 'k': 1, 'K': 1
     }
+
+    line_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
+    @staticmethod
+    def string_square_to_8x8(str_square: str):
+        return int(str_square[1]) - 1, DataSpecs.line_dict[str_square[0]]
+
 
 class Fen:
 
@@ -57,7 +67,7 @@ class Fen:
     def en_passant_target_square(self):
         return None if self.elems[3] == '-' else self.elems[3]
 
-    def transform(self, transformation_type = DataSpecs.vector6x8x8_flat):
+    def transform(self, transformation_type=DataSpecs.vector6x8x8_flat):
         if transformation_type == DataSpecs.vector6x8x8_flat:
             return self.raw_board_to_6x8x8_flat_vector()
         elif transformation_type == DataSpecs.vector12x8x8_flat:
@@ -105,7 +115,6 @@ class Fen:
         self.flip_castling()
         self.fenstring = " ".join(self.elems)
 
-
     def _reverse_piece(self, piece):
         if piece.islower():
             return piece.upper()
@@ -115,7 +124,8 @@ class Fen:
 
     def _raw_board_to_sparse_representation(self, piece_to_layer_map, piece_to_value_map, layers):
         raw_board = self.raw_board()
-        row = 0; column = 0
+        row = 0
+        column = 0
         indices_x = []
         indices_y = []
         indices_z = []
@@ -133,12 +143,13 @@ class Fen:
                 values.append(piece_to_value_map[c])
                 column = column + 1
 
-        return ({'indices_z': indices_z, 'indices_x': indices_x, 'indices_y': indices_y, 'values': values})
+        return {'indices_z': indices_z, 'indices_x': indices_x, 'indices_y': indices_y, 'values': values}
 
     def _raw_board_to_flat_vector(self, piece_to_layer_map, piece_to_value_map, layers):
         raw_board = self.raw_board()
-        row = 0; column = 0
-        vector = [0 for _ in range(0, 8*8*layers)]
+        row = 0
+        column = 0
+        vector = [0 for _ in range(0, 8 * 8 * layers)]
         for c in raw_board:
             if c.isdigit():
                 column = column + int(c)
@@ -146,6 +157,38 @@ class Fen:
                 row = row + 1
                 column = 0
             else:
-                vector[layers*(8*row + column) + piece_to_layer_map[c]] = piece_to_value_map[c]
+                vector[layers * (8 * row + column) + piece_to_layer_map[c]] = piece_to_value_map[c]
                 column = column + 1
         return vector
+
+    def castling_vector_8x8x2(self):
+        board = np.zeros([8, 8, 2])
+        board[(*DataSpecs.string_square_to_8x8('g8'), 1)] = self.castling_vector()[0]
+        board[(*DataSpecs.string_square_to_8x8('g1'), 0)] = self.castling_vector()[1]
+        board[(*DataSpecs.string_square_to_8x8('c8'), 1)] = self.castling_vector()[2]
+        board[(*DataSpecs.string_square_to_8x8('c1'), 0)] = self.castling_vector()[3]
+        return board
+
+    def en_passant_vector_8x8x1(self):
+        board = np.zeros([8, 8, 1])
+        if self.en_passant_target_square():
+            board[(*DataSpecs.string_square_to_8x8(self.en_passant_target_square), 0)] = 1
+        return board
+
+    def full_board_representation(self, dim):
+        return np.dstack((self.sparse_to_dense(dim), self.castling_vector_8x8x2, self.en_passant_vector_8x8x1))
+
+    def sparse_to_dense(self, dim):
+        board = np.zeros([8, 8, dim])
+        if dim == 6:
+            index_dict = self.raw_board_to_6x8x8_sparse_representation()
+        elif dim == 12:
+            index_dict = self.raw_board_to_12x8x8_sparse_representation()
+        else:
+            raise ValueError('Dimension not supported')
+        for coordinates in zip(index_dict['indices_x'], index_dict['indices_y'], index_dict['indices_z']):
+            board[coordinates] = 1
+        return board
+
+    def flip_color(self, full_representation):
+        return np.flip(full_representation, 0)
